d4eb3642ebf9e4f08a790267f3bc54c33e73d332
Angela Mello <157414574+anngmello@users.noreply.github.com>
2026-08-11 18:33:25 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,46 +5,44 @@ import MovieRow from './components/MovieRow';
 import FeaturedMovie from './components/FeaturedMovie';
 import Header from './components/Header';
 
-export default () => {
+const App = () => {
 
   const [movielist, setMovieList] = useState([]);
   const [featuredData, setFeaturedData] = useState(null);
   const [blackHeader, setBlackHeader] = useState(false);
 
-  useEffect (()=>{
+  useEffect(() => {
     const loadAll = async () => {
-      //Pegando a lista Total
+      // Pegando a lista Total
       let list = await Tmdb.getHomeList();
       setMovieList(list);
 
-      //Pegando o Featured
-      let originals = list.filter(i=>i.slug === 'originals');
+      // Pegando o Featured
+      let originals = list.filter(i => i.slug === 'originals');
       let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length));
       let chosen = originals[0].items.results[randomChosen];
       let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
       setFeaturedData(chosenInfo);
+    };
 
-    }
-    
-    loadAll ();   
+    loadAll();
   }, []);
 
-  useEffect (()=>{
-    const scrollListener  =  () => {
-      if(window.scrollY > 10) {
+  useEffect(() => {
+    const scrollListener = () => {
+      if (window.scrollY > 10) {
         setBlackHeader(true);
       } else {
         setBlackHeader(false);
       }
-    }
+    };
 
     window.addEventListener('scroll', scrollListener);
 
     return () => {
       window.removeEventListener('scroll', scrollListener);
-    }
+    };
   }, []);
-
 
   return (
     <div className='page'>
@@ -53,26 +51,40 @@ export default () => {
 
       {featuredData &&
         <FeaturedMovie item={featuredData} />
-      } 
+      }
 
       <section className='lists'>
-        {movielist.map((item, key)=>(
+        {movielist.map((item, key) => (
           <MovieRow key={key} title={item.title} items={item.items} />
         ))}
       </section>
 
       <footer>
-      <p>Desenvolvido por <a href="https://github.com/anngmello" target="_blank">Angela Mello</a></p>
-        Dados pegos do site themoviedb.org<br/>
+        <p>
+          Desenvolvido por{' '}
+          <a
+            href="https://github.com/anngmello"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Angela Mello
+          </a>
+        </p>
 
-
+        Dados pegos do site themoviedb.org<br />
       </footer>
 
       {movielist.length <= 0 &&
-      <div className='loading'>
-        <img src='https://i.pinimg.com/originals/9a/02/aa/9a02aac51ed499e01518ac73dd954eb1.gif' alt='Carregando' />
-      </div>
+        <div className='loading'>
+          <img
+            src="https://i.pinimg.com/originals/9a/02/aa/9a02aac51ed499e01518ac73dd954eb1.gif"
+            alt="Carregando"
+          />
+        </div>
       }
+
     </div>
   );
-} 
+};
+
+export default App;
