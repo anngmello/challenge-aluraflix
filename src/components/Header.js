fdf0a5e6f10672e5ebceff410107dd4a55a890f1
Angela Mello <157414574+anngmello@users.noreply.github.com>
2026-08-12 11:36:50 +0100

    Refactor Header component for concise syntax

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,28 +1,19 @@
 import React from "react";
 import './Header.css';
 
-const Header = ({ black }) => {
+export default ({black}) => {
     return (
-        <header className={black ? 'black' : ''}>
+        <header className= {black ? 'black' : ''}>
             <div className='header--logo'>
                 <a href="/">
-                    <img
-                        src="https://aluraflix-jade.vercel.app/static/media/Logo.5e7b3578.png"
-                        alt="Logo AluraFlix"
-                    />
+                    <img src="https://aluraflix-jade.vercel.app/static/media/Logo.5e7b3578.png" alt="Logo AluraFlix" />
                 </a>
             </div>
-
             <div className="header--user">
                 <a href="/">
-                    <img
-                        src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
-                        alt="Usuário"
-                    />
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png" alt="Usuário" />
                 </a>
             </div>
         </header>
     );
-};
-
-export default Header;
+}
